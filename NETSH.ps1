ccc2ba71988c813c0fcf6594a919282a3d9bb515
The-ScubaScott <sholtkamp32@gmail.com>
2024-12-06 10:43:44 -0500

    Add files via upload
    The NETSH.PS1 must be run as system or admin and it creates the scheduled task, creates the XML 802.1x export of the current LAN NIC, and copies the NETSH_SchedTask.ps1 to C:\Windows\Temp\NETSH_Export to be used by the scheduled task.

---
+++ NETSH.ps1
@@ -0,0 +1,106 @@
+﻿<#
+1. Create XML backup using NETSH.   Created in C:\Windows\Temp
+2. Copy Script to run to C:\Windows\Temp the script will import the XML and launched by a Scheduled Task
+3. Create schedule task to run at logon.   Script will be looking for OS version of WIN11 23H2 and will exit if false.  
+4. If WIN11 23H2 is true it will import the XML file and remove the scheduled task
+#Installation Successful: Windows successfully installed the following update: Windows 11, version 23H2
+#>
+
+
+###################################################
+### Log Write #####################################
+###################################################
+Function Log-Write
+{
+	Param ([Parameter(Mandatory = $true)]
+		[string]$LogPath,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true)]
+		[string]$LineValue)
+	
+	Process
+	{
+		$DateTime = Get-Date
+		#Add-Content -Path $LogPath -Value $LineValue
+		"$DateTime  -  $LineValue" | Out-File -FilePath $LogPath -Append
+	}
+}
+
+$LogDir = "C:\Windows\Temp\NETSH_EXPORT"
+If(!(test-path -PathType container $LogDir))
+{
+      New-Item -ItemType Directory -Path $LogDir
+}
+
+###################################################
+### Log Setup #####################################
+###################################################
+
+$DateTime = Get-Date -Format yyyyMMdd_hhmmss
+$LogDir = "C:\Windows\Temp\NETSH_Export"
+$LogFileName = "NETSH_Export"
+$LogFileType = "log"
+$FullLogPath = $LogDir + "\" + $LogFileName + "_" + $DateTime + "." + $LogFileType
+Log-Write -LogPath $FullLogPath -LineValue "Log has been configured"
+###################################################
+
+###################################################
+#Export LAN Configuration to XML
+$NICName = Get-NetAdapter | Where-Object {$_.Status -eq "Up" -and $_.InterfaceAlias -like "*Ethernet*"} | Select-Object -ExpandProperty Name
+$NICVar = $NICName
+$NICName = '"'+$NICName+'"'
+$Args = "lan export profile folder=$LogDir interface= $NICName"
+Start-Process -FilePath "netsh" -ArgumentList $Args -wait
+$Filename = $LogDir + "\" + $NICVar + ".xml"
+$Filename = "C:\Windows\Temp\NETSH_Export\Ethernet 4.xml"
+
+If (Test-Path -Path $Filename) {
+    Log-Write -LogPath $FullLogPath -LineValue "XML file created: $Filename"
+} else
+{
+    Log-Write -LogPath $FullLogPath -LineValue "XML file not created: $Filename"
+}
+
+###################################################
+
+
+
+###################################################
+#FileCopy
+net use "\\**SERVERSHARE**\it\eus\applications\scripts\NETSH"
+Copy-Item -Path "\\**SERVERSHARE**\it\eus\applications\scripts\netsh\NETSH_SchedTask.ps1" -Destination $LogDir
+$PS1FileName= $LogDir + "\NETSH_SchedTask.PS1"
+If (Test-Path $PS1Filename -PathType Leaf) {
+    Log-Write -LogPath $FullLogPath -LineValue "PS1 File copied local"
+}else
+{
+    Log-Write -LogPath $FullLogPath -LineValue "PS1 File not copied local"
+}
+###################################################
+
+
+###################################################
+#Create Scheduled Task
+$taskname = "NETSH Import LAN Config"
+$taskExists = Get-ScheduledTask | Where-Object { $_.TaskName -like $taskName }
+if ($taskExists) {
+    Log-Write -LogPath $FullLogPath -LineValue "Scheduled task already exists"
+    } 
+else 
+    {
+    $taskdescription = "Use NETSH to import the LAN Config"
+    $action = New-ScheduledTaskAction -Execute 'Powershell.exe' `
+        -Argument '-NoProfile -WindowStyle Hidden -ExecutionPolicy ByPass -File "C:\Windows\temp\NETSH_Export\NETSH_SchedTask.ps1"'
+    $trigger =  New-ScheduledTaskTrigger -AtStartup 
+    $settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Minutes 2) 
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskname -Description $taskdescription -Settings $settings -User "System"
+    }
+###################################################
+$taskExists = Get-ScheduledTask | Where-Object { $_.TaskName -like $taskName }
+if ($taskExists) {
+    Log-Write -LogPath $FullLogPath -LineValue "Scheduled task exists"
+    }
+else 
+    {
+    Log-Write -LogPath $FullLogPath -LineValue "Scheduled task was not created"
+    }
---
+++ NETSH_SchedTask.ps1
@@ -0,0 +1,86 @@
+﻿###################################################
+### Log Write #####################################
+###################################################
+Function Log-Write
+{
+	Param ([Parameter(Mandatory = $true)]
+		[string]$LogPath,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true)]
+		[string]$LineValue)
+	
+	Process
+	{
+		$DateTime = Get-Date
+		#Add-Content -Path $LogPath -Value $LineValue
+		"$DateTime  -  $LineValue" | Out-File -FilePath $LogPath -Append
+	}
+}
+
+$LogDir = "C:\Windows\Temp\NETSH_EXPORT"
+If(!(test-path -PathType container $LogDir))
+{
+      New-Item -ItemType Directory -Path $LogDir
+}
+
+###################################################
+### Log Setup #####################################
+###################################################
+
+$DateTime = Get-Date -Format yyyyMMdd_hhmmss
+$LogDir = "C:\Windows\Temp\NETSH_Export"
+$LogFileName = "NETSH_SchTask"
+$LogFileType = "log"
+$FullLogPath = $LogDir + "\" + $LogFileName + "_" + $DateTime + "." + $LogFileType
+Log-Write -LogPath $FullLogPath -LineValue "Log has been configured"
+###################################################
+
+$info = [Environment]::OSVersion.Version
+$Major = $info.Major
+$OSVersion = [System.Version]::new($Major, $info.Minor, $info.Build)
+if ($info.Build -le 22000) {
+    Log-Write -LogPath $FullLogPath -LineValue "Windows 10 $OSVersion"
+    exit
+}
+
+#start-service dot3svc
+$service = Get-Service -Name "dot3svc"
+if ($service.Status -ne "Running") {
+    # Start the service
+    Start-Service -Name "dot3svc"
+    Log-Write -LogPath $FullLogPath -LineValue "DOT3SVC was not running. Start has been requested by script"
+} else {
+    Log-Write -LogPath $FullLogPath -LineValue "DOT3SVC was already running"
+}
+
+$NICName = Get-NetAdapter | Where-Object {$_.Status -eq "Up" -and $_.InterfaceAlias -like "*Ethernet*"} | Select-Object -ExpandProperty Name
+$NIC = $NICName
+$NICName = '"'+$NICName+'"'
+$fullpath = "$($LogDir)`\$($NIC).xml"
+$Args = "lan add profile filename=`"$($fullpath)`" interface= $NICName"
+Start-Process -FilePath "netsh" -ArgumentList $Args -Wait
+Log-Write -LogPath $FullLogPath -LineValue "Executed NETSH with $Args"
+Disable-NetAdapter -Name $NIC -Confirm:$False
+Enable-NetAdapter -Name $NIC 
+Log-Write -LogPath $FullLogPath -LineValue "Disabled/re-enabled NIC"
+
+$taskname = "NETSH Import LAN Config"
+$SchPath = “C:\Windows\System32\schtasks.exe”
+$ARGS = @(
+‘/DELETE’,
+“/TN ““NETSH Import LAN Config”””,
+“/f”
+)
+
+Start-Process -FilePath $SchPath -ArgumentList $ARGS  -Wait -ErrorAction SilentlyContinue
+
+if ($(Get-ScheduledTask -TaskName $taskname -ErrorAction SilentlyContinue).TaskName -eq $taskname) { Unregister-ScheduledTask -TaskName $taskname -Confirm:$False }
+
+$taskExists = Get-ScheduledTask | Where-Object { $_.TaskName -like $taskName }
+if ($taskExists) {
+    Log-Write -LogPath $FullLogPath -LineValue "Attempted removal of Scheduled Task, still exists"
+    } 
+else 
+{
+    Log-Write -LogPath $FullLogPath -LineValue "Scheduled removed"
+    }
